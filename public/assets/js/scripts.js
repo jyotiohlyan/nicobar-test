0161ee848902afad286805107b096a3ab2907a8d
shweta ohlyan <ohlyanshweta123@gmail.com>
2019-03-18 00:22:38 +0530

    first commit of nicobar

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * GulpBootstrapBoilerplate
+ * Simple Gulp Bootstrap BoilerPlate
+ * 
+ * @author shweta ohlyan
+ * @version 1.0.5
+ * Copyright 2019. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // WRITE YOUR JQUERY HERE
+  });
+
+})(jQuery, window, document);
